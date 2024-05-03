38514a6ab18985b247f4746d4368adec41555078
FelixWSC <159850732+FelixWSC@users.noreply.github.com>
2024-05-03 12:13:27 +1200

    Version 1

---
+++ main.py
@@ -0,0 +1,20 @@
+# Ask the user their name and save it
+name = input("What is your name?")
+# Greet the user and introduce the quiz
+print("Welcome to this quiz,", name )
+print("This quiz is about different animals.")
+# Question 1
+input("What bird can fly the highest?")
+# Question 1 answer
+print("The answer is the Rüppell's Vulture")
+# Question 2
+input("What is the fastest water animal?")
+# Question 2 answer
+print("The fastest water animal is the sailfish.")
+# Question 3
+input("How many birds species are there in the world?")
+# Question 3 answer
+print("There are more than 11,000 unique bird species in the world.")
+# End the quiz
+print("Thank you for trying out my quiz.")
+
